48a7376255eae8360ef3fb322bb4cb547148575a
keito noguchi <noguchi@hug-inc.co.jp>
2026-01-28 20:52:06 +0900

    キャンペーンタイマー・日付変更

--- template/target_tokai.php
+++ template/target_tokai.php
@@ -2,10 +2,10 @@
   <div class="p-target__inner">
     <p class="p-target__head"><span><img src="<?php echo get_template_directory_uri(); ?>/assets/images/present-icon.png" alt="キャンペーンに応募する"></span> キャンペーンに<br class="u-desktop">応募する</p>
     <p class="p-target__foot">期間限定<br class="u-desktop"><span>：</span>
-      <?php if (strtotime(date('Y-m-d H:i')) < strtotime('2025-12-31 15:00')) { ?>
-        2025.12.31
-      <?php } else { ?>
+      <?php if (strtotime(date('Y-m-d H:i')) < strtotime('2026-1-31 15:00')) { ?>
         2026.1.31
+      <?php } else { ?>
+        2026.2.28
       <?php } ?></p>
   </div>
 </a>
